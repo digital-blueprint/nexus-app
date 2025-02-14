95f8ffdfea915ac6516f6813ebdaba791f99b020
zsuffa david <zsuffa.david@gmail.com>
2025-02-14 12:08:22 +0100

    extend appShell

--- src/dbp-nexus.js
+++ src/dbp-nexus.js
@@ -1,9 +1,9 @@
 // import '@webcomponents/scoped-custom-element-registry';
-import {AppShell} from '@dbp-toolkit/app-shell';
+import {NexusAppShell} from './dbp-nexus-app-shell.js';
 import * as commonUtils from '@dbp-toolkit/common/utils';
 import {Translated} from '@dbp-toolkit/common/src/translated';
 import {Translation} from '@dbp-toolkit/common/src/translation';
 
-commonUtils.defineCustomElement('dbp-nexus', AppShell);
+commonUtils.defineCustomElement('dbp-nexus', NexusAppShell);
 commonUtils.defineCustomElement('dbp-translated', Translated);
 commonUtils.defineCustomElement('dbp-translation', Translation);
