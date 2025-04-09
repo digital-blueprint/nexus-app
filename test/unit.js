e77a3d1fce103d0052e89513f986a1649419c2d0
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-09 16:03:14 +0200

    tests: remove dbp-nexus-activity test
    there is no such element

--- test/unit.js
+++ test/unit.js
@@ -4,19 +4,7 @@ import '../src/dbp-nexus-search';
 import '../src/dbp-nexus.js';
 
 suite('dbp-nexus-activity basics', () => {
-    let node;
-
-    suiteSetup(async () => {
-        node = document.createElement('dbp-nexus-activity');
-        document.body.appendChild(node);
-        await node.updateComplete;
-    });
-
-    suiteTeardown(() => {
-        node.remove();
-    });
-
-    test('should render', () => {
-        assert(node.shadowRoot !== undefined);
+    test('test', () => {
+        assert(1);
     });
 });
