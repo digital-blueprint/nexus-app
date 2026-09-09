ce4702d59ed397adb781004744c0592906d0e45e
Christoph Reiter <reiter.christoph@gmail.com>
2026-09-09 11:22:15 +0200

    Port from babel to oxc

--- rolldown.config.js
+++ rolldown.config.js
@@ -5,7 +5,6 @@ import {replacePlugin} from 'rolldown/plugins';
 import serve from 'rollup-plugin-serve';
 import license from 'rollup-plugin-license';
 import emitEJS from 'rollup-plugin-emit-ejs';
-import {getBabelOutputPlugin} from '@rollup/plugin-babel';
 import {
     getPackagePath,
     getBuildInfo,
@@ -21,7 +20,7 @@ const pkg = require('./package.json');
 const appEnv = typeof process.env.APP_ENV !== 'undefined' ? process.env.APP_ENV : 'local';
 const watch = process.env.ROLLUP_WATCH === 'true';
 const buildFull = (!watch && appEnv !== 'test') || process.env.FORCE_FULL !== undefined;
-let useBabel = buildFull;
+let transform = buildFull;
 let checkLicenses = buildFull;
 let treeshake = buildFull;
 let nodeEnv = buildFull ? 'production' : 'development';
@@ -153,6 +152,9 @@ let input = ['src/' + pkg.internalName + '.js', 'src/dbp-nexus-search.js'];
 export default (async () => {
     let privatePath = await getDistPath(pkg.name);
     return {
+        transform: {
+            target: transform ? ['chrome106', 'firefox110', 'safari16'] : 'esnext',
+        },
         input:
             appEnv != 'test'
                 ? !whitelabel
@@ -463,23 +465,6 @@ Dependencies:
                         },
                     ],
                 })),
-
-            useBabel &&
-                getBabelOutputPlugin({
-                    compact: false,
-                    presets: [
-                        [
-                            '@babel/preset-env',
-                            {
-                                shippedProposals: true,
-                                modules: false,
-                                targets: {
-                                    esmodules: true,
-                                },
-                            },
-                        ],
-                    ],
-                }),
             watch
                 ? serve({
                       contentBase: '.',
